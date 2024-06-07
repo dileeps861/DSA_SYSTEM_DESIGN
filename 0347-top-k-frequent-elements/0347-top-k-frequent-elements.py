538e479e6a6eaf069acafaf99c3c4f2a20421135
Dileep Shah <dileepshah@google.com>
2024-06-07 16:06:30 -0700

    Time: 92 ms (38.99%), Space: 21.2 MB (39.9%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,13 +1,42 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = defaultdict(int)
-        for num in nums:
-            count[num] += 1
+        counts = Counter(nums)
+        numsValues = list(counts.keys())
+        n = len(numsValues)
 
-        res = []
-        for key, val in count.items():
-            heappush(res,(val, key))
-            if len(res) > k:
-                heappop(res)
-        
-        return map(lambda x: x[1], res)
+        def partition(low, high, pivot_index):
+            pivot_frequency = counts[numsValues[pivot_index]]
+            # Move pivot to end
+            numsValues[pivot_index], numsValues[high] = (
+                numsValues[high],
+                numsValues[pivot_index],
+            )
+            store_index = low
+            for i in range(low, high):
+                if counts[numsValues[i]] < pivot_frequency:
+                    numsValues[store_index], numsValues[i] = (
+                        numsValues[i],
+                        numsValues[store_index],
+                    )
+                    store_index += 1
+            # Move pivot to its final place
+            numsValues[store_index], numsValues[high] = (
+                numsValues[high],
+                numsValues[store_index],
+            )
+            return store_index
+
+        def quickselect(low, high, k_smallest):
+            if low == high:
+                return
+            pivot_index = random.randint(low, high)
+            pivot_index = partition(low, high, pivot_index)
+            if k_smallest == pivot_index:
+                return
+            elif k_smallest < pivot_index:
+                quickselect(low, pivot_index - 1, k_smallest)
+            else:
+                quickselect(pivot_index + 1, high, k_smallest)
+
+        quickselect(0, n - 1, n - k)
+        return numsValues[n - k:]
