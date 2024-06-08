96eaf933325df7f7cf85f2b053016fb55e7ef449
Dileep Shah <dileepshah@google.com>
2024-06-07 20:15:44 -0700

    Time: 48 ms (38.58%), Space: 17 MB (86.71%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-
         i = 0
         j = len(s) - 1
         while i < j:
@@ -11,7 +9,7 @@ class Solution:
             if not s[j].isalnum():
                 j -= 1
                 continue
-            if s[i] != s[j]:
+            if s[i].lower() != s[j].lower():
                 return False
             i += 1
             j -= 1
