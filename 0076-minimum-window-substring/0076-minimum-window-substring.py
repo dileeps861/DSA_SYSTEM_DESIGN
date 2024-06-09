cf6d00d63a557c671ab9785ab9773e8d8e3572b3
Dileep Shah <dileepshah@google.com>
2024-06-08 19:16:28 -0700

    Time: 230 ms (22.53%), Space: 17.2 MB (72.56%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,11 +1,12 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         tC = Counter(t)
-        
+
         i = 0
         fres = []
         mx = float("inf")
         sC = defaultdict(int)
+
         def check():
             for key, val in tC.items():
                 if sC[key] < val:
@@ -15,17 +16,16 @@ class Solution:
         for j in range(len(s)):
             if s[j] not in tC:
                 continue
-            
+
             sC[s[j]] += 1
-            
             while i < j and (s[i] not in tC or sC[s[i]] > tC[s[i]]):
                 if s[i] in sC and sC[s[i]] > 0:
                     sC[s[i]] -= 1
                 i += 1
 
-            if check() and mx > (j-i+1):
-                mx = (j-i+1)
-                fres = [i,j]
+            if check() and mx > (j - i + 1):
+                mx = j - i + 1
+                fres = [i, j]
         if len(fres) == 0:
             return ""
-        return s[fres[0]: fres[1]+1]
+        return s[fres[0] : fres[1] + 1]
