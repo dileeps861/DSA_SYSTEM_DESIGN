4e2eb170d4551fd3ffc7fcae8b072b08ea46d82e
Dileep Shah <dileepshah@google.com>
2024-06-09 20:16:16 -0700

    Time: 229 ms (98.55%), Space: 18.2 MB (47.05%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -3,18 +3,14 @@ class Solution:
         n = len(piles)
         if h == n:
             return max(piles)
-        if n==1:
-            return ceil(piles[0]/h)
+        if n == 1:
+            return ceil(piles[0] / h)
         maxSpeed = max(piles)
         minSpeed = sum(piles) // h
 
         while minSpeed <= maxSpeed:
             midSpeed = minSpeed + (maxSpeed - minSpeed) // 2
-            hoursNeeded = 0
-            for pile in piles:
-                hoursNeeded += (
-                    pile // midSpeed if pile % midSpeed == 0 else (pile // midSpeed) + 1
-                )
+            hoursNeeded = sum([ceil(pile / midSpeed) for pile in piles])
 
             if hoursNeeded <= h:
                 maxSpeed = midSpeed - 1
