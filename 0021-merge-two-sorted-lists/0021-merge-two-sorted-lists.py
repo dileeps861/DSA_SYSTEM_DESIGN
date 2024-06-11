5276360d957e04cf31415b9506d0a75a8a57cb0d
Dileep Shah <dileepshah@google.com>
2024-06-10 22:51:14 -0700

    Time: 40 ms (45.13%), Space: 16.5 MB (88.18%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -3,28 +3,36 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # Create a dummy node to serve as the start of the merged list
-        dummy = ListNode()
-        current = dummy
-        
-        # Traverse both lists and attach the smaller node to the merged list
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        head = None
+        node = None
         while list1 and list2:
             if list1.val <= list2.val:
-                current.next = list1
-                list1 = list1.next
+                if not head:
+                    head = list1
+                    node = head
+                else:
+                    node.next = list1
+                    node = node.next
+                list1 = list1.next 
             else:
-                current.next = list2
+                if not head:
+                    head = list2
+                    node = head
+                else:
+                    node.next = list2
+                    node = node.next
+
                 list2 = list2.next
-            current = current.next
-        
-        # Attach the remaining nodes from list1 or list2
+            
+            
         if list1:
-            current.next = list1
-        elif list2:
-            current.next = list2
-        
-        # Return the merged list starting from the next of the dummy node
-        return dummy.next
+            node.next = list1
+        if list2:
+            node.next = list2
+        return head
