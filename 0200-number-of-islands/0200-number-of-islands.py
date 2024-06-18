38fb8c442287863e3eeee44dbd189228f68162d9
Dileep Shah <dileepshah@google.com>
2024-06-18 01:15:59 -0700

    Time: 250 ms (41.53%), Space: 18.7 MB (95.05%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        res = 0
+        DIRS = [[0,1],[1,0],[-1,0],[0,-1]]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    grid[i][j] = '0'
+                    q = deque([(i,j)])
+
+                    while q:
+                        k,l = q.pop()
+                        for u,v in DIRS:
+                            m,n = k + u, l + v
+                            if m >=0 and m < len(grid) and n >= 0 and n < len(grid[m]) and grid[m][n] == '1':
+                                q.append((m,n))
+                                grid[m][n] = '0'
+                    res += 1
+        return res
+
