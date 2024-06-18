2882590e662abf49e360f38064baaf1da5f91b2b
Dileep Shah <dileepshah@google.com>
2024-06-17 17:23:06 -0700

    Time: 2688 ms (90.77%), Space: 16.6 MB (18.39%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -11,23 +11,24 @@ class Solution:
             if idx == len(word):
                 ans[0] = True
                 return
-        
-            for u,v in dirs:
-                k,l = i + u, j + v
+
+            for u, v in dirs:
+                k, l = i + u, j + v
                 if k >= 0 and k < n and l >= 0 and l < m and board[k][l] == word[idx]:
                     char = board[k][l]
                     board[k][l] = "*"
-                    dfs(k,l, idx + 1)
+                    dfs(k, l, idx + 1)
                     board[k][l] = char
                     if ans[0]:  # Stop further recursion if word is found
                         return
+
         for i in range(n):
             for j in range(m):
                 if board[i][j] != word[0]:
                     continue
                 char = board[i][j]
                 board[i][j] = "*"
-                dfs(i,j, 1)
+                dfs(i, j, 1)
                 board[i][j] = char
                 if ans[0]:
                     return True
