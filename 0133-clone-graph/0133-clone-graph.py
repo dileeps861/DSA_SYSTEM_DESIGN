b9312c2fe41d2faf06c3f219fcc224cc3a030979
Dileep Shah <dileepshah@google.com>
2024-06-18 18:03:00 -0700

    Time: 52 ms (5.8%), Space: 16.9 MB (71.13%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -12,16 +12,16 @@ class Solution:
     def __init__(self):
         self.visited = dict()
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if node in self.visited:
-            return self.visited[node]
         if not node:
             return node
-        nodeClone = Node(node.val)
-        if not node.neighbors:
-            return nodeClone
-        nodeClone.neighbors = []
+        
+        if node in self.visited:
+            return self.visited[node]
+        
+        nodeClone = Node(node.val, [])
         self.visited[node] = nodeClone
-       
-        for child in node.neighbors:
-            nodeClone.neighbors.append(self.cloneGraph(child))
+        
+        for neighbor in node.neighbors:
+            nodeClone.neighbors.append(self.cloneGraph(neighbor))
+        
         return nodeClone
