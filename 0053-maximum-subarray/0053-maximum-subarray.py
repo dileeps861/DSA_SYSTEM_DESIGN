9a264158e559aea5ac2a0212b983b8b42d6f2445
Dileep Shah <dileepshah@google.com>
2024-06-26 22:22:38 -0700

    Time: 553 ms (27.02%), Space: 31.1 MB (28.64%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        res = float("-inf")
+        
         n = len(nums)
 
         # Brute force O(n^2)
@@ -12,13 +12,18 @@ class Solution:
 
         #  Optimal solution, start left=right = 0
         # keep moving forward and if the total sum becomes less than nums[right] upon adding new element and reset total sum = nums[right] otherwise add nums to totalSum
-        right = 0
-        totalSum = 0
-        while right < n:
-            if totalSum + nums[right] < nums[right]:
-                totalSum = 0
-            totalSum += nums[right]
-            res = max(res, totalSum)
-            right += 1
+        # right = 0
+        # totalSum = 0
+        # while right < n:
+        #     if totalSum + nums[right] < nums[right]:
+        #         totalSum = 0
+        #     totalSum += nums[right]
+        #     res = max(res, totalSum)
+        #     right += 1
 
+        # Another approach kadane's
+        res = currSum = nums[0]
+        for i in range(1, n):
+            currSum = max(nums[i], nums[i] + currSum)
+            res = max(res, currSum)
         return res
