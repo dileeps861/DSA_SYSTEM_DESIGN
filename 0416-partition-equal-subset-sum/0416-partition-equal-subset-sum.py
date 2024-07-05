af8ca8f40058cbd8051a87d24c1aadf2a2dc0296
Dileep Shah <dileepshah@google.com>
2024-07-04 18:49:40 -0700

    Time: 566 ms (62.34%), Space: 16.6 MB (90.04%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -11,19 +11,32 @@ class Solution:
         n = len(nums)
         if total % 2 != 0:
             return False
-        dp = [[None]*(half+1) for _ in range(n)]
-        def findPartition(i, currSum):
-            if i >= n:
-                if currSum == half:
-                    return True
-                return False
-            if currSum == half:
-                dp[i][currSum] = True
-                return True
-            if currSum > half:
-                return False
-            if dp[i][currSum] != None:
-                return dp[i][currSum]
-            dp[i][currSum] = findPartition(i+1, currSum + nums[i]) or findPartition(i+1, currSum)
-            return dp[i][currSum]
-        return findPartition(0, 0)
+        # dp = [None for _ in range(n)]
+        # def findPartition(i, currSum):
+        #     if i >= n:
+        #         if currSum == half:
+        #             return True
+        #         return False
+        #     if currSum == half:
+        #         dp[i][currSum] = True
+        #         return True
+        #     if currSum > half:
+        #         return False
+        #     if dp[i][currSum] != None:
+        #         return dp[i][currSum]
+        #     dp[i][currSum] = findPartition(i+1, currSum + nums[i]) or findPartition(i+1, currSum)
+        #     return dp[i][currSum]
+        # return findPartition(0, 0)
+
+        # Furthure optimization: We can optimize space as all we need to know is with given sum
+        # is it possible to make res sum
+
+        dp = [False] * (half + 1)
+        dp[0] = True
+        
+        # Update the DP array
+        for num in nums:
+            for j in range(half, num - 1, -1):
+                dp[j] = dp[j] or dp[j - num]
+        
+        return dp[half]
