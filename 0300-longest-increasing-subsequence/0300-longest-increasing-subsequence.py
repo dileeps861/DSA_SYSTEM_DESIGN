c9313530dbb75e4ab6bae8940ca2e35dae22c14c
Dileep Shah <dileepshah@google.com>
2024-07-06 16:40:03 -0700

    Time: 67 ms (87.04%), Space: 17 MB (29.6%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -3,12 +3,27 @@ class Solution:
         if not nums:
             return 0
         
-        n = len(nums)
-        dp = [1] * n  # Initialize DP array with 1s because the minimum LIS is 1 (each element by itself)
-        res = 1
-        for i in range(1, n):
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-                    res = max(res, dp[i])
-        return res
+        # n = len(nums)
+        # dp = [1] * n  # Initialize DP array with 1s because the minimum LIS is 1 (each element by itself)
+        # res = 1
+        # for i in range(1, n):
+        #     for j in range(i):
+        #         if nums[j] < nums[i]:
+        #             dp[i] = max(dp[i], dp[j] + 1)
+        #             res = max(res, dp[i])
+        # return res
+
+        # further optimization use binary search
+        import bisect
+        lisList = []
+        for num in nums:
+            if not lisList:
+                lisList.append(num)
+                continue
+            idx = bisect.bisect_left(lisList, num)
+            if idx == len(lisList):
+                lisList.append(num)
+            else:
+                lisList[idx] = num
+        return len(lisList)
+
