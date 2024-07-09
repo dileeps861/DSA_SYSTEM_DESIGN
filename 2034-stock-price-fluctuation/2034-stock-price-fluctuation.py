f946bd0b0d1326c224e7daf3cc231be852c69019
Dileep Shah <dileepshah@google.com>
2024-07-08 19:26:39 -0700

    Time: 740 ms (10.43%), Space: 57.9 MB (91.57%) - LeetHub

---
+++ 2034-stock-price-fluctuation/2034-stock-price-fluctuation.py
@@ -0,0 +1,37 @@
+class StockPrice:
+
+    def __init__(self):
+        from sortedcontainers import SortedDict
+        self.stockPrices = SortedDict()
+        self.sortedPrice = SortedDict()
+
+    def update(self, timestamp: int, price: int) -> None:
+        # print(timestamp, price)
+        if timestamp in self.stockPrices:
+            old = self.stockPrices[timestamp]
+            if self.sortedPrice[old] > 1:
+                self.sortedPrice[old] = self.sortedPrice[old] - 1
+            else:
+                del self.sortedPrice[old]
+        if price not in self.sortedPrice:
+            self.sortedPrice[price] = 0
+        self.sortedPrice[price] = self.sortedPrice[price] + 1
+        self.stockPrices[timestamp] = price
+
+    def current(self) -> int:
+        # print("curr:", self.stockPrices.peekitem(-1)[0])
+        return self.stockPrices.peekitem(-1)[1]
+
+    def maximum(self) -> int:
+        return self.sortedPrice.peekitem(-1)[0]
+
+    def minimum(self) -> int:
+        return self.sortedPrice.peekitem(0)[0]
+
+
+# Your StockPrice object will be instantiated and called as such:
+# obj = StockPrice()
+# obj.update(timestamp,price)
+# param_2 = obj.current()
+# param_3 = obj.maximum()
+# param_4 = obj.minimum()
