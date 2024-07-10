e4e152300a6dfa40f7f9c7656ce53e84d54e55f6
Dileep Shah <dileepshah@google.com>
2024-07-10 03:48:19 -0700

    Time: 74 ms (75.78%), Space: 19.9 MB (11.56%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        head = ListNode()
+        prev = head
+        pq = []
+        count = 0
+        for ls in lists:
+            if ls:
+                tup = (ls.val, count, ls)
+                count += 1
+                heappush(pq, tup)
+        while pq:
+            count += 1
+            val, _, node = heappop(pq)
+            prev.next = node
+            if node.next:
+                heappush(pq, (node.next.val, count, node.next))
+            prev = node
+
+        return head.next
