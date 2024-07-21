39d51a69cab1541b4c8f2a4e6b53f7052b4cc0ef
Dileep Shah <dileepshah@google.com>
2024-07-21 13:24:40 -0700

    Time: 54 ms (79.63%), Space: 16.8 MB (16.22%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,15 +1,22 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s2) < len(s1):
-            return False
-        s1Map = Counter(s1)
+        charsMap1 = [0] * 26
+        for char in s1:
+            ordChar = ord(char) - ord("a")
+            charsMap1[ordChar] += 1
+
+        charsMap = [0] * 26
         i = 0
-        s2Map = defaultdict(int)
         for j in range(len(s2)):
-            s2Map[s2[j]] += 1
-            while i <j and s2[j] not in s1Map or s2Map[s2[j]] > s1Map[s2[j]]:
-                s2Map[s2[i]] -= 1
+            ordChar = ord(s2[j]) - ord("a")
+            charsMap[ordChar] += 1
+
+            while charsMap[ordChar] > charsMap1[ordChar] and i <= j:
+                ordChar_i = ord(s2[i]) - ord("a")
+                charsMap[ordChar_i] -= 1
                 i += 1
-            if (j - i + 1) == len(s1):
-                return True
+
+            if j >= i and (j - i + 1) == len(s1):
+                if eq(charsMap, charsMap1):
+                    return True
         return False
