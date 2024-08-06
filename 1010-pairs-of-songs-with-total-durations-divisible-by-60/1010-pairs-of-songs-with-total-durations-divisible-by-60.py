bcf90ae3ab4f673eb80d7c305059d1675897424a
Dileep Shah <dileepshah@google.com>
2024-08-06 01:06:47 -0700

    Time: 219 ms (79.83%), Space: 20.9 MB (59.2%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,7 +1,7 @@
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
 
-        cache = {}
+        cache = [0] * 60
         res = 0
         for num in time:
             rem = num % 60
@@ -11,11 +11,7 @@ class Solution:
             else:
                 comp = 60 - rem
 
-            if comp in cache:
-                res += cache[comp]
-
-            if rem not in cache:
-                cache[rem] = 0
+            res += cache[comp]
             cache[rem] += 1
 
         return res
