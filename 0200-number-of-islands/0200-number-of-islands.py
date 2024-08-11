4f7317212faaaf4e9ec285976c0ddf99ca4965b5
Dileep Shah <dileepshah@google.com>
2024-08-11 09:52:32 -0700

    Time: 241 ms (62.16%), Space: 18.7 MB (99.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        res = 0
-        DIRS = [[0, 1], [1, 0], [-1, 0], [0, -1]]
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        n = len(grid)
+        noOfIslands = 0
+        for i in range(n):
+            m = len(grid[i])
+            for j in range(m):
                 if grid[i][j] == "1":
-                    grid[i][j] = "0"
-                    q = deque([(i, j)])
-
-                    while q:
-                        k, l = q.pop()
-                        for u, v in DIRS:
-                            m, n = k + u, l + v
-                            if (
-                                m >= 0
-                                and m < len(grid)
-                                and n >= 0
-                                and n < len(grid[m])
-                                and grid[m][n] == "1"
-                            ):
-                                q.append((m, n))
-                                grid[m][n] = "0"
-                    res += 1
-        return res
+                    noOfIslands += 1
+                    stack = []
+                    stack.append((i, j))
+                    while stack:
+                        p, q = stack.pop(-1)
+                        for r, s in dirs:
+                            u, v = p + r, q + s
+                            if 0 <= u < n and 0 <= v < m and grid[u][v] == "1":
+                                grid[u][v] = "0"
+                                stack.append((u, v))
+        return noOfIslands
