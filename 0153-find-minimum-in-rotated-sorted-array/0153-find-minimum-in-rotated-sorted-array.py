c9a1ce1e7603ff2a9adb7950ae71c5e7220a1763
Dileep Shah <dileepshah@google.com>
2024-08-16 00:06:11 -0700

    Time: 36 ms (91.3%), Space: 16.8 MB (42.2%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -9,7 +9,6 @@ class Solution:
             if nums[low] <= midVal and midVal <= nums[high]:
                 return nums[low]
             if mid == low and mid == high:
-                # print(mid)
                 return nums[mid]
             if midVal >= nums[low] and midVal >= nums[mid - 1]:
                 low = mid + 1
