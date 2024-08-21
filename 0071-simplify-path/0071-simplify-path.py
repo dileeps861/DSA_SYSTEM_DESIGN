ee86cb12c8f6b7eaf4e416ad09afb26a66daf5c3
Dileep Shah <dileepshah@google.com>
2024-08-20 21:45:29 -0700

    Time: 44 ms (14.78%), Space: 16.4 MB (99.34%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,24 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        pathSpl = path.split('/')
+        
+        res = []
+        i = 0
+        while i < len(pathSpl):
+            pt = pathSpl[i]
+            i += 1
+            if not pt:
+                continue
+            elif '.' == pt:
+                continue
+            elif pt == '..':
+                if res:
+                    res.pop(-1)
+            else:
+                res.append(pt)
+            
+
+        res = '/'.join(res)
+        if not res:
+            return '/'
+        return '/' + res
