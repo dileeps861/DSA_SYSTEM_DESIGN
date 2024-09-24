c6488a4c303fbe459a509587af86c4a2d01246e4
Dileep Shah <dileepshah@google.com>
2024-09-24 02:43:30 -0700

    Time: 69 ms (87.22%), Space: 19.9 MB (17.96%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,42 +1,28 @@
 class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        # Approach: is find a place in the list where start of the interval is the floor of the start of interval which is less than the new interval and then try to insert that interval in the list and then keep going up if there is a start date greater than the end date of the new interval so that we can merge those intervals so I think this could be done in log n time because log n time plus the overlapping intervals so I think this could be done in log n time.
-        if not intervals:
-            return [newInterval]
+    def insert(
+        self, intervals: List[List[int]], newInterval: List[int]
+    ) -> List[List[int]]:
+        res = []
+        i = 0
+        # Add all intervals that come before the new interval
+        while i < len(intervals) and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
 
-        def findBisectPoint():
-            left = 0
-            right = len(intervals) - 1
+        # Merge all overlapping intervals
+        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
+            newInterval = [
+                min(newInterval[0], intervals[i][0]),
+                max(newInterval[1], intervals[i][1]),
+            ]
+            i += 1
 
-            while left <= right:
-                mid = left + (right - left) // 2
-                if intervals[mid][0] >= newInterval[0]:
-                    right = mid - 1
-                else:
-                    left = mid + 1
+        # Add the merged interval (or the new interval if no merge happened)
+        res.append(newInterval)
 
-            return left    
-        
-        # Find the correct position to insert newInterval using binary search
-        pos = findBisectPoint()
+        # Add the remaining intervals after the new interval
+        while i < len(intervals):
+            res.append(intervals[i])
+            i += 1
 
-        # Merge the intervals
-        merged = []
-        # Add all intervals before the insertion point
-        for i in range(pos):
-            merged.append(intervals[i])
-        
-        # Add and merge the new interval
-        if not merged or merged[-1][1] < newInterval[0]:
-            merged.append(newInterval)
-        else:
-            merged[-1][1] = max(merged[-1][1], newInterval[1])
-        
-        # Merge remaining intervals
-        for i in range(pos, len(intervals)):
-            if merged[-1][1] < intervals[i][0]:
-                merged.append(intervals[i])
-            else:
-                merged[-1][1] = max(merged[-1][1], intervals[i][1])
-
-        return merged
+        return res
