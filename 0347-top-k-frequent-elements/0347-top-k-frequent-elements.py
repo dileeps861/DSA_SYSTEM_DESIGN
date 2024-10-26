37c3cb83b3de135f174cf6850c80acf46ddd92a5
Dileep Shah <dileepshah@google.com>
2024-10-26 14:07:19 -0700

    Time: 0 ms (100%), Space: 19.9 MB (99.88%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,6 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         from collections import Counter
-        freq = Counter(nums)
-        sortedFreq = sorted(freq.items(), key=lambda x: (-x[1], [0]))
-        return map(lambda x: x[0], sortedFreq[:k])
+        numCount = Counter(nums)
+        pq = []
+        for num, count in numCount.items():
+            heappush(pq, (count, num))
+            if len(pq) > k:
+                heappop(pq)
+        res = []
+        while pq:
+            res.append(heappop(pq)[1])
+        return res
+
