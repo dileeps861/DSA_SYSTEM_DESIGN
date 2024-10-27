2de18dd8de1e5f5b18687b4c0c97ce512a6c3795
Dileep Shah <dileepshah@google.com>
2024-10-27 00:34:57 -0700

    Time: 107 ms (61.68%), Space: 25.9 MB (98.95%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,16 +1,16 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         # add num and idx
-        res = [0] * len(temperatures)
-        i = len(temperatures) - 1
+        n = len(temperatures)
+        res = [0] * n
+
         ms = []
-        while i >= 0:
+        for i in range(n - 1, -1, -1):
             while ms and temperatures[ms[-1]] <= temperatures[i]:
                 ms.pop()
             if ms:
                 idx = ms[-1]
                 res[i] = idx - i
             ms.append(i)
-            i -= 1
 
         return res
