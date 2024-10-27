6191909255de37d0696a630a1a278abf15291925
Dileep Shah <dileepshah@google.com>
2024-10-26 22:04:31 -0700

    Time: 178 ms (20.33%), Space: 77.7 MB (79.57%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,52 +1,63 @@
-class Node:
-    def __init__(self, value, key):
-        self.value = value
-        self.key = key
-        self.next = None
-        self.prev = None
-
 class LRUCache:
+    class ListNode:
+        def __init__(self):
+            self.val = None
+            self.key = None
+            self.next = None
+            self.prev = None
+
     def __init__(self, capacity: int):
         self.cache = {}
-        self.head = Node(-1, -1)
-        self.tail = Node(-1, -1)
+        self.head = ListNode()
+        self.tail = ListNode()
+        self.head.next = self.tail
         self.tail.prev = self.head
-        self.head.next = self.tail 
         self.capacity = capacity
 
-    def insert(self, node):
-        oldNode = self.tail.prev
-        node.next = self.tail
-        self.tail.prev = node
-        oldNode.next = node
-        node.prev = oldNode
-
-    def remove(self, node):
+    # remove the entry from the linked list()
+    def delete(self, node):
         prev = node.prev
-        prev.next = node.next
         node.next.prev = prev
+        prev.next = node.next
+
+    def add(self, node):
+
+        prevNode = self.tail.prev
+        prevNode.next = node
+        node.prev = prevNode
+        self.tail.prev = node
+        node.next = self.tail
+
+    # add the entry to the linked list
 
     def get(self, key: int) -> int:
+        # if not present return -1
+        # if yes, upadate the usage by geting the existing key and value
+        # then delete the pair and then again add it to the cache, only need to update linked list()
         if key not in self.cache:
             return -1
-        self.remove(self.cache[key])
-        self.insert(self.cache[key])
-
-        return self.cache[key].value
-        
+        node = self.cache[key]
+        self.delete(node)
+        self.add(node)
+        return node.val
 
     def put(self, key: int, value: int) -> None:
-        node = Node(value, key)
+        # if already exists the the key update its value by first removing it
+        # then add it to the cache i.e. linked list()
+        # then if the size becomes more than the capacity remove the head and move head next
+        node = ListNode()
+        node.val = value
+        node.key = key
         if key in self.cache:
-            self.remove(self.cache[key])
+            self.delete(self.cache[key])
 
+        self.add(node)
         self.cache[key] = node
-        self.insert(node)
-        
+
         if len(self.cache) > self.capacity:
-            del self.cache[self.head.next.key]
-            self.remove(self.head.next)
-            
+            lru_node = self.head.next
+            del self.cache[lru_node.key]
+            self.delete(lru_node)
 
 
 # Your LRUCache object will be instantiated and called as such:
