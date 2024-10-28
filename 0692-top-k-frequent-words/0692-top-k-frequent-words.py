70888bbfb570d41b6dd6c27294e5da6dfabf06ca
Dileep Shah <dileepshah@google.com>
2024-10-27 23:01:20 -0700

    Time: 0 ms (100%), Space: 16.7 MB (36.37%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -2,7 +2,7 @@ class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         cMap = Counter(words)
         cMapSorted = sorted(cMap.items(), key=lambda x: (-x[1], x[0]))
-        print(cMapSorted)
+        # print(cMapSorted)
         i = 0
         res = []
         while i < k and len(res) < k:
