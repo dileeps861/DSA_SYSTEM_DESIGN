7a476ecb6ec74fddc0f49a9e83da295f76f587c9
Dileep Shah <dileepshah@google.com>
2024-10-28 22:02:42 -0700

    Time: 408 ms (12.12%), Space: 25.7 MB (98.59%) - LeetHub

---
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxMoves(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        dirs = [[-1, -1], [0, -1], [1, -1]]
+        res = 0
+        dp = [0] * rows
+
+        for col in range(1, cols):
+            dpcp = [0] * rows
+            for row in range(rows):
+                for dr, dc in dirs:
+                    u, v = row + dr, col + dc
+                    if 0 <= u < rows and 0 <= v < cols and grid[row][col] > grid[u][v]:
+                        if col > 1 and dp[u] == 0:
+                            continue
+                        dpcp[row] = max(dpcp[row], dp[u] + 1)
+                res = max(res, dpcp[row])
+            
+            dp = dpcp
+            # print(dp)
+
+        return res
