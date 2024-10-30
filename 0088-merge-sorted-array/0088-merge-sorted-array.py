3414ad52ad3cdb67bb39ca332d51320a6a59ac6c
Dileep Shah <dileepshah@google.com>
2024-10-30 13:59:34 -0700

    Time: 0 ms (100%), Space: 16.7 MB (8.61%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,14 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        k = len(nums1) - 1
         i = m - 1
         j = n - 1
-        while k >= 0 and j >= 0:
-            if i < 0 or nums1[i] < nums2[j]:
-                nums1[k] = nums2[j]
-                j -= 1
-            else:
+        for k in range(len(nums1) - 1, -1, -1):
+            if j < 0:
+                break
+            if i >= 0 and nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
                 i -= 1
-            k -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
