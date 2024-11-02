0e3890eac5eb86a9c5531c32f7077a0c5ccdf26d
Dileep Shah <dileepshah@google.com>
2024-11-02 00:19:21 -0700

    Time: 4 ms (54.51%), Space: 19.1 MB (8.4%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,65 +1,74 @@
-from collections import defaultdict
-from typing import List
+import unittest
+
 
 class Solution:
-    # def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-    #     adj = defaultdict(list)
-
-    #     for u, v in prerequisites:
-    #         adj[v].append(u)
-
-    #     visited = [0] * numCourses  # 0 = unvisited, 1 = visiting, 2 = visited
-    #     ans = [True]
-    #     stack = []
-
-    #     def dfs(u):
-    #         if visited[u] == 1:  # Cycle detected
-    #             ans[0] = False
-    #             return
-    #         if visited[u] == 2:  # Already fully processed
-    #             return
-    #         visited[u] = 1  # Mark as visiting
-    #         for v in adj[u]:
-    #             if visited[v] != 2:  # Only visit if not fully processed
-    #                 dfs(v)
-    #                 if not ans[0]:  # Early stop if cycle detected
-    #                     return
-    #         visited[u] = 2  # Mark as fully processed
-    #         stack.append(u)
-
-    #     for u in range(numCourses):
-    #         if visited[u] == 0:
-    #             dfs(u)
-    #         if not ans[0]:  # Early stop if cycle detected
-    #             break
-        
-    #     if not ans[0]:
-    #         return []
-        
-    #     return stack[::-1]
-
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj = defaultdict(list)
-        in_degree = [0] * numCourses
-        
-        for u, v in prerequisites:
-            adj[v].append(u)
-            in_degree[u] += 1
+        # build order of the courses to take,
+        # for each course build its pre req course relation
+        # initiate the traversal with the items having 0 in degrees, zero dependencies
+        # keep doing so and reduce in degree when a cours is taken
 
-        # Queue of all nodes with in-degree 0
-        queue = deque([i for i in range(numCourses) if in_degree[i] == 0])
-        result = []
+        # courses a-> b, b->c, c->d b -> d a-> c
+        # queue based impl will be?
+        # intiailly build adj map with nodes dependent on the nodes
+        # then build in-degree map
+        # then add all the nodes with 0 in degree add it to queue
+        # take all those initial courses and reduce each one's in degree and as sson asin degrees
+        # beomes 0 add that course to queue, maintain a list to add courses in result
+        # if len(res) != numCourses return 0 else num course
+        res = []
 
-        while queue:
-            node = queue.popleft()
-            result.append(node)
-            for neighbor in adj[node]:
-                in_degree[neighbor] -= 1
-                if in_degree[neighbor] == 0:
-                    queue.append(neighbor)
-
-        # If the number of courses in the result is less than numCourses, there is a cycle
-        if len(result) == numCourses:
-            return result
-        else:
+        adj = {}
+        indegs = {}
+        for dC, pC in prerequisites:
+            if pC not in adj:
+                adj[pC] = set()
+            if dC not in indegs:
+                indegs[dC] = 0
+            indegs[dC] += 1
+            adj[pC].add(dC)
+        # print(adj)
+        # print(indegs)
+        q = Deque()
+        for c in range(numCourses):
+            if c not in indegs or indegs[c] == 0:
+                q.append(c)
+        # print(q)
+        while q:
+            sz = len(q)
+            while sz > 0:
+                c = q.popleft()
+                res.append(c)
+                if c not in adj:
+                    sz -= 1
+                    continue
+                for cD in adj[c]:
+                    if cD in indegs:
+                        indegs[cD] -= 1
+                        if indegs[cD] == 0:
+                            q.append(cD)
+                sz -= 1
+        if len(res) != numCourses:
             return []
+        return res
+
+
+class UnitTestSol(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def testWithStraightRelationship(self):
+        res = self.sol.findOrder(3, [[2, 1], [1, 0]])
+        self.assertEquals([], res)
+
+    def testWithCircularRelationship(self):
+        res = self.sol.findOrder(3, [[2, 1], [1, 0], [0, 2]])
+        self.assertEquals([], res)
+
+    def testWithComplexButPossible(self):
+        res = self.sol.findOrder(4, [[2, 1], [1, 0], [2, 0], [3, 2]])
+        self.assertEquals([], res)
+
+    def testWithSingleCourse(self):
+        res = self.sol.findOrder(1, [])
+        self.assertEquals([0], res)
