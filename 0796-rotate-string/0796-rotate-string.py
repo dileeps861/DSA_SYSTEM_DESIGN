0426ea04867e65a0f7257e47448701e92ba27982
Dileep Shah <dileepshah@google.com>
2024-11-02 22:35:12 -0700

    Time: 0 ms (100%), Space: 16.5 MB (77.64%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        # Kind of Brute force appraoch
+        # for idx, char in enumerate(s):
+        #     if s[idx] == goal[0]:
+        #         # print(s[idx:] , goal[0: len(goal) - idx - 1] , s[0: idx] , goal[len(goal) - idx:])
+        #         if s[idx:] == goal[0: len(goal) - idx] and s[0: idx] == goal[len(goal) - idx:]:
+        #             return True
+
+        # return False
+
+        # Optimal apparoach:
+        # S+S then do substring search using KMP will help to find
+        if len(goal) != len(s):
+            return False
+        return goal in (s + s)
