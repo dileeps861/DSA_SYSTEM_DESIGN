be7ab0231fa9b054a63aedbee6ca1215c3bcfd56
Dileep Shah <dileepshah@google.com>
2024-11-02 23:51:09 -0700

    Time: 0 ms (100%), Space: 16.5 MB (63.93%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -4,28 +4,32 @@ class Solution:
         left, right = 0, len(matrix[0])
 
         flattenedItems = []
+
+        def traversAndAdd(start, stop, step, isRow, fixIdx):
+            for idx in range(start, stop, step):
+                if isRow:
+                    flattenedItems.append(matrix[fixIdx][idx])
+                else:
+                    flattenedItems.append(matrix[idx][fixIdx])
+
         while left < right and top < bottom:
 
             # Fill top row
-            for idx in range(left, right):
-                flattenedItems.append(matrix[top][idx])
+            traversAndAdd(left, right, 1, True, top)
             top += 1
             # Fill right column
-            for idx in range(top, bottom):
-                flattenedItems.append(matrix[idx][right - 1])
+            traversAndAdd(top, bottom, 1, False, right - 1)
             right -= 1
 
-            # Case when ther is just left one row or one column then in that case we need to break
-            # To avoid considering same row and columns again
+            # Case when ther is just left one row or one column then in that case we need 
+            # to break To avoid considering same row and columns again
             if not left < right or not top < bottom:
                 break
             # Fill bottom row
-            for idx in range(right - 1, left - 1, -1):
-                flattenedItems.append(matrix[bottom - 1][idx])
+            traversAndAdd(right - 1, left - 1, -1, True, bottom - 1)
             bottom -= 1
 
             # Fill left column
-            for idx in range(bottom - 1, top - 1, -1):
-                flattenedItems.append(matrix[idx][left])
+            traversAndAdd(bottom - 1, top - 1, -1, False, left)
             left += 1
         return flattenedItems
