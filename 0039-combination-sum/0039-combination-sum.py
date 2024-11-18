9a63d4bc6dc8baff3c4fadcca8adc065256ba890
Dileep Shah <dileepshah@google.com>
2024-11-17 23:38:27 -0800

    Time: 11 ms (63.99%), Space: 16.8 MB (14.5%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -14,7 +14,7 @@ class Solution:
             lst.append(candidates[i])
             dfs(i, lst, currSum + candidates[i])
             lst.pop(-1)  # backtracking the list
-            while i + 1 < n and candidates[i + 1] == candidates[i]:
+            if i + 1 < n and candidates[i + 1] == candidates[i]:
                 # Skip the repeating nums to avoid duplicate combiantions
                 i += 1
             # dont take the num
