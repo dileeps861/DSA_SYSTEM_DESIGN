fedfabcf1bfebc6528b6f191acd6e0978d6d8561
Dileep Shah <dileepshah@google.com>
2025-04-06 23:35:59 -0700

    Time: 617 ms (57.43%), Space: 158.3 MB (12.3%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -15,5 +15,5 @@ class Solution:
             return dfs(i + 1, curr_sum + nums[i]) or dfs(i + 1, curr_sum)
 
         # Use tuple to ensure immutability for caching
-        nums = tuple(nums)
+        nums
         return dfs(0, 0)
