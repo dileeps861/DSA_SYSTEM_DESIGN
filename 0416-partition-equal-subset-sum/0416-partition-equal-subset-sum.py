59d696417c5d08baa34e9b7b8f1941ba4bdbb9ae
Dileep Shah <dileepshah@google.com>
2025-04-06 23:32:18 -0700

    Time: 548 ms (61.66%), Space: 71.7 MB (23.17%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,42 +1,23 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        # first sort the number 
-        # get total sum
-        # if total odd return immidiatl false
-        # at any point if we have curr sum == total//2 return true
-        # at any point we have two choices, take ith number of skipp it
-        nums.sort()
-        total = sum(nums)
-        half = total // 2
-        n = len(nums)
-        if total % 2 != 0:
+        @lru_cache(maxsize=None)
+        def dfs(nums: Tuple[int], n: int, subset_sum: int) -> bool:
+            # Base cases
+            if subset_sum == 0:
+                return True
+            if n == 0 or subset_sum < 0:
+                return False
+            result = (dfs(nums, n - 1, subset_sum - nums[n - 1])
+                    or dfs(nums, n - 1, subset_sum))
+            return result
+
+        # find sum of array elements
+        total_sum = sum(nums)
+
+        # if total_sum is odd, it cannot be partitioned into equal sum subsets
+        if total_sum % 2 != 0:
             return False
-        # dp = [None for _ in range(n)]
-        # def findPartition(i, currSum):
-        #     if i >= n:
-        #         if currSum == half:
-        #             return True
-        #         return False
-        #     if currSum == half:
-        #         dp[i][currSum] = True
-        #         return True
-        #     if currSum > half:
-        #         return False
-        #     if dp[i][currSum] != None:
-        #         return dp[i][currSum]
-        #     dp[i][currSum] = findPartition(i+1, currSum + nums[i]) or findPartition(i+1, currSum)
-        #     return dp[i][currSum]
-        # return findPartition(0, 0)
 
-        # Furthure optimization: We can optimize space as all we need to know is with given sum
-        # is it possible to make res sum
-
-        dp = [False] * (half + 1)
-        dp[0] = True
-        
-        # Update the DP array
-        for num in nums:
-            for j in range(half, num - 1, -1):
-                dp[j] = dp[j] or dp[j - num]
-        
-        return dp[half]
+        subset_sum = total_sum // 2
+        n = len(nums)
+        return dfs(tuple(nums), n - 1, subset_sum)
