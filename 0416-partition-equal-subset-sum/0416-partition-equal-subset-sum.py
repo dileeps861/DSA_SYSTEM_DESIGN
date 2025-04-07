11b9717be6c0b7a1b2496e24d90bd74d0bfd4313
Dileep Shah <dileepshah@google.com>
2025-04-06 23:47:37 -0700

    Time: 486 ms (72.68%), Space: 17.9 MB (84.54%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,17 +1,34 @@
 class Solution:
+    # def canPartition(self, nums: List[int]) -> bool:
+    #     total = sum(nums)
+    #     if total % 2 != 0:
+    #         return False
+    #     target = total // 2
+
+    #     @lru_cache(maxsize=None)
+    #     def dfs(i, curr_sum):
+    #         if curr_sum == target:
+    #             return True
+    #         if i >= len(nums) or curr_sum > target:
+    #             return False
+    #         # take or skip
+    #         return dfs(i + 1, curr_sum + nums[i]) or dfs(i + 1, curr_sum)
+
+    #     return dfs(0, 0)
+
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
         if total % 2 != 0:
             return False
         target = total // 2
 
-        @lru_cache(maxsize=None)
-        def dfs(i, curr_sum):
-            if curr_sum == target:
-                return True
-            if i >= len(nums) or curr_sum > target:
-                return False
-            # take or skip
-            return dfs(i + 1, curr_sum + nums[i]) or dfs(i + 1, curr_sum)
+        # dp[i] = True if a subset sum of i is possible
+        dp = [False] * (target + 1)
+        dp[0] = True  # 0 is always possible
 
-        return dfs(0, 0)
+        for num in nums:
+            # Traverse backwards to avoid using the same num multiple times
+            for i in range(target, num - 1, -1):
+                dp[i] = dp[i] or dp[i - num]
+
+        return dp[target]
