578713b4cfbdd521cb6f1d3daf2aa2a7e45a6987
Dileep Shah <dileepshah@google.com>
2025-05-31 12:01:29 -0700

    Time: 245 ms (62.58%), Space: 20.3 MB (43.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,20 +1,27 @@
 class Solution:
+
     def numIslands(self, grid: List[List[str]]) -> int:
-        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        n = len(grid)
-        noOfIslands = 0
-        for i in range(n):
-            m = len(grid[i])
-            for j in range(m):
+        dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        m, n = len(grid), len(grid[0])
+        num_of_islands = 0
+
+        def is_a_valid_move(i, j):
+            return i >= 0 and j >= 0 and i < m and j < n and grid[i][j] == "1"
+
+        def find_connected_lands(i, j):
+            grid[i][j] = "2"
+            for dir_i, dir_j in dirs:
+                new_i, new_j = i + dir_i, j + dir_j
+                if is_a_valid_move(new_i, new_j):
+                    find_connected_lands(new_i, new_j)
+
+        for i in range(m):
+
+            for j in range(n):
+
                 if grid[i][j] == "1":
-                    noOfIslands += 1
-                    stack = []
-                    stack.append((i, j))
-                    while stack:
-                        p, q = stack.pop(-1)
-                        for r, s in dirs:
-                            u, v = p + r, q + s
-                            if 0 <= u < n and 0 <= v < m and grid[u][v] == "1":
-                                grid[u][v] = "0"
-                                stack.append((u, v))
-        return noOfIslands
+                    # do something recusrively to find connected land
+                    find_connected_lands(i, j)
+                    num_of_islands += 1
+
+        return num_of_islands
