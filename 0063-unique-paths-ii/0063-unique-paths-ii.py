26049be92297ec7569c3e0798a74ec0b16dc8340
Dileep Shah <dileepshah@google.com>
2025-09-09 14:29:22 -0700

    Time: 3 ms (22.89%), Space: 18.3 MB (10.44%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -11,16 +11,14 @@ class Solution:
         def dfs(i, j):
             if i == m-1 and j == n-1:
                 return 1
-            # obstacleGrid[i][j] = 1
+
             if (i, j) in memo:
                 return memo[(i, j)]
             memo[(i, j)] = 0
             for u, v in dirs:
                 new_i, new_j = i + u, j + v
                 if is_valid_move(new_i, new_j):
-                    # obstacleGrid[i][j] = 2
                     memo[(i, j)] += dfs(new_i, new_j)
-                    # obstacleGrid[i][j] = 0
             return memo[(i, j)] 
         if obstacleGrid[0][0] == 0:
             return dfs(0, 0)
