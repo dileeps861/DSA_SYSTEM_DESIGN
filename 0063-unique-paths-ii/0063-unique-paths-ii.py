8594d6812223651efa31e5fe952516cba620b1fe
Dileep Shah <dileepshah@google.com>
2025-09-09 14:26:46 -0700

    Time: 7 ms (2.53%), Space: 18.4 MB (10.44%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        dirs = [[0, 1], [1, 0]]
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+
+        memo = {}
+        def is_valid_move(i, j):
+            return 0 <= i < m and 0 <= j <n and obstacleGrid[i][j] == 0
+
+        def dfs(i, j):
+            if i == m-1 and j == n-1:
+                return 1
+            # obstacleGrid[i][j] = 1
+            if (str(i) +":"+str(j)) in memo:
+                return memo[(str(i) +":"+str(j))]
+            memo[(str(i) +":"+str(j))] = 0
+            for u, v in dirs:
+                new_i, new_j = i + u, j + v
+                if is_valid_move(new_i, new_j):
+                    obstacleGrid[i][j] = 2
+                    memo[(str(i) +":"+str(j))] += dfs(new_i, new_j)
+                    obstacleGrid[i][j] = 0
+            return memo[(str(i) +":"+str(j))] 
+        if obstacleGrid[0][0] == 0:
+            return dfs(0, 0)
+        return 0
