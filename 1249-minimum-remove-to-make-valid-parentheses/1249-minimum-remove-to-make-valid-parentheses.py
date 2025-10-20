0c9b09a92a75c435361fc9e15a630910d0b0957f
Dileep Shah <dileepshah@google.com>
2025-10-19 17:05:43 -0700

    Time: 54 ms (27.85%), Space: 19.1 MB (79.98%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -15,11 +15,4 @@ class Solution:
         for val in stack:
             dict_map[val] = True
 
-        res = []
-
-        for i in range(len(s)):
-            if i in dict_map:
-                continue
-            else:
-                res.append(s[i])
-        return ''.join(res)
+        return ''.join(char for i, char in enumerate(s) if i not in dict_map)
